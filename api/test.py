6b626b745f04b72bac717c487fb3bfc300701e37
AlbinNosskire <albin.eriksson2@elev.ga.ntig.se>
2021-11-17 09:48:18 +0100

    local test for api

---
+++ api/test.py
@@ -0,0 +1,18 @@
+import requests
+import json
+
+
+url = 'http://localhost:8080/address/22' #22 is id for request
+address = {'address': 'Kummingatan'}
+
+response = requests.post(url, json=address)
+
+response = requests.get(url)
+response_json = response.json()
+
+print("Response was " + str(response_json))
+if response_json == {"22": {"address": "Kummingatan", "id": 22}}:
+    print("Test was successful")
+
+else:
+    print("Test faild")
