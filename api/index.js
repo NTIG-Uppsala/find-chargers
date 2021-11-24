91878d7ffb228d3aec45aab866ace8ce4688e036
AlbinNosskire <albin.eriksson2@elev.ga.ntig.se>
2021-11-24 12:27:51 +0100

    Add append function to api post

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
+const fs = require('fs');
 
 app.listen(
     PORT,
@@ -10,19 +11,30 @@ app.listen(
 app.use( express.json() )
 
 app.post('/address/:id', (req, res) => {
-
     const {id} = req.params;
     const {address} = req.body;
-    const address_data = `{"${id}": {"address": "${address}", "id": ${id}}}`;
-
-    writeToJson(address_data);
-    return res.send(address_data);
+    let rawdata = fs.readFileSync('address.json');
+    let address_data = JSON.parse(rawdata);
+    let in_json = address_data.addresses.map(in_json => {
+        if (in_json.id == id) {
+            return true;
+        }    
+    });
+    if (in_json.includes(true)) {
+        return res.send('id is already in use');
+        
+    }
+    address_data['addresses'].push({
+        'id': id,
+        'address': address
+    });
+    writeToJson(JSON.stringify(address_data));
+    return res.send('Data successfully saved');
 });
 
 app.get('/address/:id', (req, res) => {
 
     const {id} = req.params;
-    const fs = require('fs');
 
     let rawdata = fs.readFileSync('address.json');
     let data = JSON.parse(rawdata);
@@ -36,7 +48,6 @@ app.get('/address/:id', (req, res) => {
 });
 
 async function writeToJson(new_address) {
-    const fs = require('fs');
     fs.writeFile('address.json', new_address, (err) => {
         if (err) throw err;
         console.log('The file has been saved!');
