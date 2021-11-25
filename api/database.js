a1676c70edf507b3cd703971304688011616c646
Jonatan Möne <jonatan.mone@elev.ga.ntig.se>
2021-11-25 15:19:07 +0100

    Added Azure documentation.

--- api/database.js
+++ api/database.js
@@ -1,20 +1,23 @@
 let mysql = require('mysql');
 let conn = mysql.createConnection({
   // Databas uppgifter
-  host: 'Localhost',
-  user: 'root',
+  host: 'find-chargers-database.mysql.database.azure.com',
+  user: 'Ntindivid',
   password: '02_gula_tankar',
   database: 'findchargers'
 });
 
-code = "insert_data";
-address_name = "Purjogatan 67";
+let code = "insert_data";
+let address_name = "Purjogatan 67";
+let coordinates = [59.858593, 17.638674]
+let coordinate_lat = coordinates[0];
+let coordinate_lon = coordinates[1];
 
 if (code == "get_data"){
   sql = "SELECT * FROM charger";
 }
 else if (code == "insert_data"){
-  sql = `INSERT INTO charger(address) VALUES("${address_name}");`;
+  sql = `INSERT INTO charger(address, coordinate_lat, coordinate_lon) VALUES("${address_name}", ${coordinate_lat}, ${coordinate_lon});`;
 }
 
 conn.connect(function(err) {
---
+++ documentation/azure.md
@@ -0,0 +1,7 @@
+## Azure setup
+
+The following actions have already been taken and must not be repeated, unless previous work has been undone.
+Begin by making a Microsoft Azure account [from here](https://azure.microsoft.com/en-us/free/). Be sure to use the github student packet if you have it.
+When you have made your account you must must make an app service. This will require you to make a resource group as well. After this simply follow these tutorials:
+https://docs.microsoft.com/sv-se/azure/app-service/quickstart-nodejs?pivots=development-environment-vscode&tabs=windows
+https://docs.microsoft.com/sv-se/azure/app-service/deploy-continuous-deployment?tabs=github
