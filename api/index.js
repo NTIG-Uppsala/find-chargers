c0b743b4ccb700cf4d407cdeb34edfe4734f934f
AlbinNosskire <89513315+AlbinNosskire@users.noreply.github.com>
2021-11-25 15:00:56 +0100

    Work branch 1 (#7)
    * Put DoD and DE in a folder
    
    * Updated README.
    
    * Small fix in README.
    
    * API first version
    
    * small syntax fixes
    
    * local test for api
    
    * Added get_data code.
    
    * Add workflow to auto test api
    
    * Fixet python test not able to failed
    
    * Made it possible to add data to the local database via js code.
    
    * Create push_to_server.yml
    
    * Update push_to_server.yml
    
    * Add append function to api post
    
    * Added more test data
    
    * Added function to for coordinates and so you can get location of closest locations
    
    * Added node_module to calculate distance on coordinates
    
    * Removed unused code
    
    * changed ++i on row 47
    
    * Fixed test for new data structure
    
    Co-authored-by: Jonatan Möne <jonatan.mone@elev.ga.ntig.se>
    Co-authored-by: Jonatan Möne <89516211+Jonatan-Mone@users.noreply.github.com>

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
+const fs = require('fs');
 
 app.listen(
     PORT,
@@ -10,35 +11,58 @@ app.listen(
 app.use( express.json() )
 
 app.post('/address/:id', (req, res) => {
-
     const {id} = req.params;
     const {address} = req.body;
-    const address_data = `{"${id}": {"address": "${address}", "id": ${id}}}`;
-
-    writeToJson(address_data);
-    return res.send(address_data);
+    const {coordinates} = req.body;
+    let rawdata = fs.readFileSync('address.json');
+    let address_data = JSON.parse(rawdata);
+    let in_json = address_data.addresses.map(in_json => {
+        if (in_json.id == id) {
+            return true;
+        }    
+    });
+    if (in_json.includes(true)) {
+        return res.send('id is already in use');
+        
+    }
+    address_data['addresses'].push({
+        'id': id,
+        'address': address,
+        'coordinates': coordinates
+    });
+    writeToJson(JSON.stringify(address_data));
+    return res.send('Data successfully saved');
 });
 
-app.get('/address/:id', (req, res) => {
+app.get('/address/:lat/:long/:max_distance', (req, res) => {
 
-    const {id} = req.params;
-    const fs = require('fs');
+    const {lat} = req.params;
+    const {long} = req.params;
+    const {max_distance} = req.params;
+    const list_to_send = [];
 
     let rawdata = fs.readFileSync('address.json');
-    let data = JSON.parse(rawdata);
+    let address_data = JSON.parse(rawdata);
 
-    try {
-        return res.send(data); 
-    } 
-    catch {
-        return res.send(`No name found on id ${id}`);
+    for (i = 0; i < address_data['addresses'].length; i++) {     
+        let distance = calculate_distance(address_data['addresses'][i].coordinates, [lat, long]);
+        if (distance < max_distance){
+            list_to_send.push(address_data['addresses'][i]);
+        }
     }
+    return res.send(list_to_send);
+
 });
 
 async function writeToJson(new_address) {
-    const fs = require('fs');
     fs.writeFile('address.json', new_address, (err) => {
         if (err) throw err;
         console.log('The file has been saved!');
     });
+}
+
+function calculate_distance(cords1, cords2) {
+    const calculate = require('calculate-coordinates');
+    let result = calculate.fromCoordinatesReturningM(cords1, cords2);
+    return result
 }
