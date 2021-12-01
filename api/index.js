21183386df56d2de8d91de1b3040e85d4faf9871
AlbinNosskire <albin.eriksson2@elev.ga.ntig.se>
2021-12-01 10:22:15 +0100

    Changed so that the api communicates with database instead of local file

--- api/index.js
+++ api/index.js
@@ -3,68 +3,84 @@ const app = express();
 const PORT = 8080;
 const fs = require('fs');
 
+let mysql = require('mysql');
+let conn = mysql.createConnection({
+  // Databas uppgifter
+  host: 'charger-database.mysql.database.azure.com',
+  user: 'Ntindivid',
+  password: '02_gula_tankar',
+  database: 'findchargers'
+});
+
 // listen for api calls
 app.listen(
     PORT,
     () => console.log(`API started local on port ${PORT}`)
 )
 
+// makes api able to read json format
 app.use( express.json() )
 
-// post function for rest api that appends new charger
-app.post('/address/:id', (req, res) => {
-    const {id} = req.params;
-    const {address} = req.body;
-    const {coordinates} = req.body;
-    let rawdata = fs.readFileSync('address.json');
-    let address_data = JSON.parse(rawdata);
-    // return true if id is already used
-    let in_json = address_data.addresses.map(in_json => {
-        if (in_json.id == id) {
-            return true;
-        }    
-    });
-    if (in_json.includes(true)) {
-        return res.send('id is already in use');
-    }
-    // formating of data before appending new data
-    address_data['addresses'].push({
-        'id': id,
-        'address': address,
-        'coordinates': coordinates
-    });
-    // write to data file
-    writeToJson(JSON.stringify(address_data));
-    return res.send('Data successfully saved');
-});
-
 // find all chargers in range of position
-app.get('/address/:lat/:long/:max_distance', (req, res) => {
-
+app.get('/get-chargers-in-range/:lat/:long/:max_distance', (req, res) => {
     const {lat} = req.params;
     const {long} = req.params;
     const {max_distance} = req.params;
     const list_to_send = [];
+    let sql = 'SELECT * FROM charger';
 
-    let rawdata = fs.readFileSync('address.json');
-    let address_data = JSON.parse(rawdata);
-    // filter chargers in range
-    for (i = 0; i < address_data['addresses'].length; i++) {     
-        let distance = calculate_distance(address_data['addresses'][i].coordinates, [lat, long]);
-        if (distance < max_distance){
-            list_to_send.push(address_data['addresses'][i]);
+    conn.query(sql, function (err, result) {
+        if (err) throw err;
+        // filter chargers in range
+        for (i = 0; i < result.length; i++) {     
+            let distance = calculate_distance([result[i].coordinate_lat, result[i].coordinate_long], [lat, long]);
+            if (distance < max_distance){
+                list_to_send.push(result[i]);
+            }
         }
-    }
-    return res.send(list_to_send);
-
+        return res.send(list_to_send);
+    });
 });
 
-async function writeToJson(new_address) {
-    fs.writeFile('address.json', new_address, (err) => {
+// adds data to the database by sending it in json format
+app.post('/post-charger', (req, res) => {
+    const {body} = req;
+    let sql = `INSERT INTO charger(
+        address, 
+        coordinate_lat, 
+        coordinate_long, 
+        email_address, 
+        ac_1, 
+        ac_2, 
+        chademo, 
+        ccs, 
+        user_input) 
+        VALUES(
+            "${body.address_name}", 
+            ${body.coordinate_lat}, 
+            ${body.coordinate_long}, 
+            "${body.email_address}", 
+            ${body.ac_1}, 
+            ${body.ac_2}, 
+            ${body.chademo}, 
+            ${body.ccs}, 
+            "${body.user_input}");`;
+            
+    conn.query(sql, function (err, result) {
         if (err) throw err;
-        console.log('The file has been saved!');
+        return res.send(body);
     });
-}
+    
+});
+
+// gets all charges in the database
+app.get('/get-charger', (req, res) => {
+    let sql = 'SELECT * FROM charger';
+    conn.query(sql, function (err, result) {
+        if (err) throw err;
+        return res.send(result);
+    });  
+})
 
 // calculates distance between two coordinate points(returns in meters)
 function calculate_distance(cords1, cords2) {
