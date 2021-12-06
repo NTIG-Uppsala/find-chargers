84f7f629415cebda25c9ad946bcb4626267d8a7a
AlbinNosskire <albin.eriksson2@elev.ga.ntig.se>
2021-12-06 11:12:37 +0100

    Added test that changed charger to active

--- tests/pytes.py
+++ tests/pytes.py
@@ -8,3 +8,9 @@ class TestClass:
             pass
         else:
             raise Exception("Failed")
+    def test_two(self):
+        request_anwser = requests.put("https://find-chargers.azurewebsites.net/change-charger-visibility/20/1/test@test.com")
+        if request_anwser == "Charger changed to active":
+            pass
+        else:
+            raise Exception("Failed")
