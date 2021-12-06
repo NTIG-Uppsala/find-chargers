5e7bff0d341a0932eea0979c20c06ec9f30edb57
AlbinNosskire <albin.eriksson2@elev.ga.ntig.se>
2021-12-06 15:15:11 +0100

    Added test if new charger is visible

--- tests/pytes.py
+++ tests/pytes.py
@@ -1,52 +1,70 @@
 import pytest
 import requests
 
-class TestClass:
-    def non_existing_charger_active(self):
-        request_anwser = requests.put("https://find-chargers.azurewebsites.net/change-charger-visibility/1/1/test@test.com")
-        if request_anwser == "Charger dose not exist":
-            pass
-        else:
-            raise Exception("Failed")
-    def non_existing_charger_inactive(self):
-        request_anwser = requests.put("https://find-chargers.azurewebsites.net/change-charger-visibility/1/0/test@test.com")
-        if request_anwser == "Charger dose not exist":
-            pass
-        else:
-            raise Exception("Failed")
-    def charger_inactive(self):
-        request_anwser = requests.put("https://find-chargers.azurewebsites.net/change-charger-visibility/20/0/test@test.com")
-        if request_anwser == "Charger changed to inactive":
-            pass
-        else:
-            raise Exception("Failed")
-    def charger_active(self):
-        request_anwser = requests.put("https://find-chargers.azurewebsites.net/change-charger-visibility/20/1/test@test.com")
-        if request_anwser == "Charger changed to active":
-            pass
-        else:
-            raise Exception("Failed")
-    def search_for_active(self):
-        request_anwser = requests.get("https://find-chargers.azurewebsites.net/get-chargers-in-range/30.5/20/500")
-        if request_anwser.json()[-1]["user_input"] == "Testing":
-            pass
-        else:
-            raise Exception("Charger not found")
-    def send_minus1(self):
-        request_anwser = requests.put("https://find-chargers.azurewebsites.net/change-charger-visibility/20/-1/test@test.com")
-        if request_anwser == "Use 1 or 0 to activate/inactivate":
-            pass
-        else:
-            raise Exception("Failed")
-    def send_2(self):
-        request_anwser = requests.put("https://find-chargers.azurewebsites.net/change-charger-visibility/20/2/test@test.com")
-        if request_anwser == "Use 1 or 0 to activate/inactivate":
-            pass
-        else:
-            raise Exception("Failed")
-    def incorrect_email(self):
-        request_anwser = requests.put("https://find-chargers.azurewebsites.net/change-charger-visibility/20/1/test2 @test.com")
-        if request_anwser == "Email or id is incorrect":
-            pass
-        else:
-            raise Exception("Failed")
+
+def test_non_existing_charger_active():
+    request_anwser = requests.put("https://find-chargers.azurewebsites.net/change-charger-visibility/1/1/test@test.com")
+    if request_anwser == "Charger dose not exist":
+        pass
+    else:
+        raise Exception("Failed")
+def test_non_existing_charger_inactive():
+    request_anwser = requests.put("https://find-chargers.azurewebsites.net/change-charger-visibility/1/0/test@test.com")
+    if request_anwser == "Charger dose not exist":
+        pass
+    else:
+        raise Exception("Failed")
+def test_charger_inactive():
+    request_anwser = requests.put("https://find-chargers.azurewebsites.net/change-charger-visibility/20/0/test@test.com")
+    if request_anwser == "Charger changed to inactive":
+        pass
+    else:
+        raise Exception("Failed")
+def test_charger_active():
+    request_anwser = requests.put("https://find-chargers.azurewebsites.net/change-charger-visibility/20/1/test@test.com")
+    if request_anwser == "Charger changed to active":
+        pass
+    else:
+        raise Exception("Failed")
+def test_search_for_active():
+    request_anwser = requests.get("https://find-chargers.azurewebsites.net/get-chargers-in-range/30.5/20/500")
+    if request_anwser.json()[-1]["user_input"] == "Testing":
+        pass
+    else:
+        raise Exception("Charger not found")
+def test_send_minus1():
+    request_anwser = requests.put("https://find-chargers.azurewebsites.net/change-charger-visibility/20/-1/test@test.com")
+    if request_anwser == "Use 1 or 0 to activate/inactivate":
+        pass
+    else:
+        raise Exception("Failed")
+def test_send_2():
+    request_anwser = requests.put("https://find-chargers.azurewebsites.net/change-charger-visibility/20/2/test@test.com")
+    if request_anwser == "Use 1 or 0 to activate/inactivate":
+        pass
+    else:
+        raise Exception("Failed")
+def test_incorrect_email():
+    request_anwser = requests.put("https://find-chargers.azurewebsites.net/change-charger-visibility/20/1/test2@test.com")
+    if request_anwser == "Email or id is incorrect":
+        pass
+    else:
+        raise Exception("Failed")
+def test_new_charger_can_be_found():
+    data = {
+    "address": "Testgatan 65",
+    "coordinate_lat": 31.5,
+    "coordinate_long": 21,
+    "ac_1": 0,
+    "ac_2": 0,
+    "chademo": 1,
+    "ccs": 1,
+    "user_input": "Testing",
+    "email_address": "test@test.com"
+    }
+    request_anwser = requests.post("https://find-chargers.azurewebsites.net/post-charger", json=data).json()
+    request_anwser2 = requests.get("https://find-chargers.azurewebsites.net/get-chargers-in-range/31.5/21/100").json()
+    if request_anwser["insertId"] == request_anwser2[-1]["id"]:
+        pass
+    else:
+        raise Exception("Failed")
