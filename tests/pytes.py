da200de5f27e25c33e9395e222c73d310afbd4ea
AlbinNosskire <albin.eriksson2@elev.ga.ntig.se>
2021-12-06 14:23:28 +0100

    Added test to finde active charger

--- tests/pytes.py
+++ tests/pytes.py
@@ -2,21 +2,27 @@ import pytest
 import requests
 
 class TestClass:
-    def test_one(self):
+    def non_existing_charger_active(self):
         request_anwser = requests.put("https://find-chargers.azurewebsites.net/change-charger-visibility/1/1/test@test.com")
         if request_anwser == "Charger dose not exist":
             pass
         else:
             raise Exception("Failed")
-    def test_two(self):
+    def charger_inactive(self):
+        request_anwser = requests.put("https://find-chargers.azurewebsites.net/change-charger-visibility/20/0/test@test.com")
+        if request_anwser == "Charger changed to inactive":
+            pass
+        else:
+            raise Exception("Failed")
+    def charger_active(self):
         request_anwser = requests.put("https://find-chargers.azurewebsites.net/change-charger-visibility/20/1/test@test.com")
         if request_anwser == "Charger changed to active":
             pass
         else:
             raise Exception("Failed")
-    def test_five(self):
-        request_anwser = requests.put("https://find-chargers.azurewebsites.net/change-charger-visibility/20/1/test@test.com")
-        if request_anwser == "Charger changed to active":
+    def search_for_active(self):
+        request_anwser = requests.get("https://find-chargers.azurewebsites.net/get-chargers-in-range/30.5/20/500")
+        if request_anwser.json()[-1]["user_input"] == "Testing":
             pass
         else:
-            raise Exception("Failed")
+            raise Exception("Charger not found")
