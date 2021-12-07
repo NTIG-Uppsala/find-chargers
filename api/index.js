00f17c9be6f86cfa437b842b3fd22eb77757f96e
liljekvist <mikael0225@outlook.com>
2021-12-07 13:28:50 +0100

    Added express-validator to escape and validate input

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const { check, body, validationResult } = require('express-validator');
 const PORT = 8080;
 const fs = require('fs');
 
@@ -22,10 +23,15 @@ app.listen(
 app.use( express.json() )
 
 // find all chargers in range of position
-app.get('/get-chargers-in-range/:lat/:long/:max_distance', (req, res) => {
-    const {lat} = req.params;
-    const {long} = req.params;
-    const {max_distance} = req.params;
+app.get('/get-chargers-in-range/:lat/:long/:max_distance',[
+    check('lat').isNumeric().trim().escape(),
+    check('long').isNumeric().trim().escape(),
+    check('max_distance').isNumeric().trim().escape()
+  ],
+   (req, res) => {
+    const lat = req.params.lat;
+    const long = req.params.long;
+    const max_distance = req.params.max_distance;
     const list_to_send = [];
     let sql = 'SELECT * FROM charger';
 
@@ -48,7 +54,23 @@ app.get('/get-chargers-in-range/:lat/:long/:max_distance', (req, res) => {
 });
 
 // adds data to the database by sending it in json format
-app.post('/post-charger', (req, res) => {
+app.post('/post-charger', [ 
+    body('email_address', "Must be a valid email!").isEmail().normalizeEmail(),
+    body('address', "Must not be empty!").not().isEmpty().escape(),
+    body('coordinate_lat', "Must be a number!").isNumeric().trim().escape(),
+    body('coordinate_long', "Must be a number!").isNumeric().trim().escape(),
+    body('ac_1').toBoolean().escape(), //gör en string som är "true" eller "false". Inte en faktisk bool!
+    body('ac_2').toBoolean().escape(),
+    body('chademo').toBoolean().escape(),
+    body('ccs').toBoolean().escape(),
+    body('user_input').escape()
+
+ ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
     const {body} = req;
     let sql_1 = `INSERT INTO charger(
         address, 
@@ -78,7 +100,7 @@ app.post('/post-charger', (req, res) => {
             if (err) throw err;
             conn.query(sql_2, function (err, result) {
                 if (err) throw err;
-                return res.send(body);
+                return res.send(result);
             });
         });
     }
@@ -102,8 +124,15 @@ app.get('/get-charger', (req, res) => {
     }
 });
 
-app.get('/get-charger-by-email/:email', (req, res) => {
-    const {email} = req.params;
+app.get('/get-charger-by-email/:email', [
+    check('email', "Must be a valid email!").isEmail().normalizeEmail()
+  ],
+   (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const email = req.params.email;
     let sql = `SELECT * FROM charger WHERE id IN (
             SELECT id FROM email WHERE email_address = "${email}"
             );`;
@@ -118,9 +147,17 @@ app.get('/get-charger-by-email/:email', (req, res) => {
     }
 });
 
-app.delete('/delete-charger-by-id/:id/:email', (req, res) => {
-    const {id} = req.params;
-    const {email} = req.params;
+app.delete('/delete-charger-by-id/:id/:email',[
+    check('email', "Must be a valid email!").isEmail().normalizeEmail(),
+    check('id', "Must be a number!").isNumeric().trim().escape()
+  ],
+   (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    const id = req.params.id;
+    const email = req.params.email;
     let id_exist = false
     let sql1 = `SELECT * FROM charger WHERE id IN (
         SELECT id FROM email WHERE email_address = "${email}"
@@ -151,22 +188,31 @@ app.delete('/delete-charger-by-id/:id/:email', (req, res) => {
     }
 });
 
-app.put('/change-charger-visibility/:id/:is_visible/:email', (req, res) => {
-    const {id} = req.params;
-    const {is_visible} = req.params;
-    const {email} = req.params;
-    let id_exist = false
+app.put('/change-charger-visibility/:id/:is_visible/:email', [
+    check('email', "Must be a valid email!").isEmail().normalizeEmail(),
+    check('id', "Must be a number!").isNumeric().trim().escape(),
+    check('is_visible').toBoolean().escape() //gör en string som är "true" eller "false". Inte en faktisk bool!
+  ], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    const id = req.params.id;
+    const is_visible = req.params.is_visible;
+    const email = req.params.email;
+    let visibility_status;
+    let id_exist = false;
     let sql1 = `SELECT * FROM charger WHERE id IN (
         SELECT id FROM email WHERE email_address = "${email}"
         );`;
-    let sql2 = `UPDATE charger SET is_visible = ${is_visible} WHERE id = ${id}`
-    if (is_visible == 1 || is_visible == 0) {
+    let sql2 = `UPDATE charger SET is_visible = ${is_visible} WHERE id = ${id}`;
+    try{
         conn.query(sql1, function (err, result) {
             if (err) throw err;
             for (i = 0; i < result.length; i++) { 
                 if (result[i].id == id){
                     id_exist = true
-                    const visibility_status = result[i]['is_visible'];
+                    visibility_status = result[i]['is_visible'];
                     break
                 }
             }
@@ -174,11 +220,11 @@ app.put('/change-charger-visibility/:id/:is_visible/:email', (req, res) => {
                 if (visibility_status != is_visible) {
                     conn.query(sql2, function (err) {
                         if (err) throw err;
-                        if (is_visible == 0) {
-                            return res.send('Charger changed to inactive')
+                        if (is_visible == "true") {
+                            return res.send('Charger changed to active');
                         }
                         else {
-                            return res.send('Charger changed to active')
+                            return res.send('Charger changed to inactive');
                         }
                     });
                 }
@@ -188,8 +234,8 @@ app.put('/change-charger-visibility/:id/:is_visible/:email', (req, res) => {
             }
         });
     }
-    else {
-        return res.send('Use 1 or 0 to activate/inactivate')
+    catch(err){
+        console.error(err);
     }
 });
 
