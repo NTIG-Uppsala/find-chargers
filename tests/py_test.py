09ff661d062260339155307f464a5b2e4e9f9412
AlbinNosskire <albin.eriksson2@elev.ga.ntig.se>
2021-12-13 11:06:41 +0100

    Changed to assert and fixed edge case on tests

--- tests/py_test.py
+++ tests/py_test.py
@@ -23,9 +23,8 @@ def test_check_charger_by_email():
     request_answer = requests.get(url=url+"get-charger-by-email/test@test.com").json()
     for i in range(0, len(request_answer)):
         if request_answer[i]["id"] == charger_id:     
-            pass
             return
-    raise Exception(request_answer)
+    assert request_answer[i]["id"] == charger_id
 
 # Check charger in range
 def test_check_charger_in_range():
@@ -33,9 +32,8 @@ def test_check_charger_in_range():
     request_answer = requests.get(url=url+"get-chargers-in-range/" + str(data["coordinate_lat"] + 0.00486) + "/" + str(data["coordinate_long"]) + "/" + "10000").json() # Find charger in 1000m range while standing ~500m away
     for i in range(0, len(request_answer)):
         if request_answer[i]["id"] == charger_id:     
-            pass
             return
-    raise Exception(request_answer)
+    assert request_answer[i]["id"] == charger_id
 
 # Check charger not in range
 def test_check_charger_not_in_range():
@@ -46,30 +44,28 @@ def test_check_charger_not_in_range():
     else:
         for i in range(0, len(request_answer)):
             if request_answer[i]["id"] == charger_id:          
-                raise Exception(request_answer)
-        pass
+                assert not request_answer[i]["id"] == charger_id
 
 # Delete charger and check deletion
 def test_delete_charger():
     charger_id = post_request()
     requests.delete(url=url+"delete-charger-by-id/" + str(charger_id) + "/test@test.com")
     request_answer = requests.get(url=url+"get-charger-by-email/test@test.com").json()
-    for i in range(0, len(request_answer)):
-        if request_answer[i]["id"] == charger_id:     
-            raise Exception(request_answer)
-    pass
+    if request_answer == []:
+        pass
+    else:
+        for i in range(0, len(request_answer)):
+            if request_answer[i]["id"] == charger_id:          
+                assert not request_answer[i]["id"] == charger_id
 
 # Send non number to chargers in range
 def test_non_number_distance():
     expected_answer = {"errors": [{"value": "hej\"--","msg": "Must be a number!","param": "max_distance","location": "params"}]}
     request_answer = requests.get(url=url+'get-chargers-in-range/31.5/21/hej"--')
-    if request_answer.json() == expected_answer:
-        pass
-    else:
-        raise Exception(request_answer)
+    assert request_answer.json() == expected_answer
 
 # Test wierd data when posting
-def test_wired_data_post():
+def test_wierd_data_post():
     send_data = {
 	"address": "Kamomillgatan 8",
 	"coordinate_lat": "hej hej",
@@ -82,82 +78,59 @@ def test_wired_data_post():
     }
     expected_answer = {"errors": [{"value": 4653262,"msg": "Must be a valid email!","param": "email_address","location": "body"},{"value": "hej hej","msg": "Must be a number!","param": "coordinate_lat", "location": "body"}]}
     request_answer = requests.post(url=url+"post-charger", json=send_data)
-    if request_answer.json() == expected_answer:
-        pass
-    else:
-        raise Exception(request_answer)
+    assert request_answer.json() == expected_answer
 
 # Trys to activet non exsisting charger
 def test_non_existing_charger_active():
     request_answer = requests.put(url=url+"change-charger-visibility/1/1/test@test.com")
-    if request_answer.text == "Email or id is incorrect":
-        pass
-    else:
-        raise Exception(request_answer)
+    assert request_answer.text == "Email or id is incorrect"
 
 # Trys to inactivet non exsisting charger
 def test_non_existing_charger_inactive():
     request_answer = requests.put(url=url+"change-charger-visibility/1/0/test@test.com")
-    if request_answer.text == "Email or id is incorrect":
-        pass
-    else:
-        raise Exception(request_answer)
+    assert request_answer.text == "Email or id is incorrect"
 
 # Set charger to inactive
 def test_charger_inactive():
     charger_id = post_request()
     request_answer = requests.put(url=url+"change-charger-visibility/" + str(charger_id) + "/0/test@test.com")
-    if request_answer.text == "Charger changed to inactive":
-        pass
-    else:
-        raise Exception(request_answer)
+    assert request_answer.text == "Charger changed to inactive"
 
 # Set charger to active
 def test_charger_active():
     charger_id = post_request()
     request_answer = requests.put(url=url+"change-charger-visibility/" + str(charger_id) + "/0/test@test.com")
     request_answer = requests.put(url=url+"change-charger-visibility/" + str(charger_id) + "/1/test@test.com")
-    if request_answer.text == "Charger changed to active":
-        pass
-    else:
-        raise Exception(request_answer)
+    assert request_answer.text == "Charger changed to active"
 
 # Looks to not finde inactive charger
 def test_search_for_inactive():
     charger_id = post_request()
     requests.put(url=url+"change-charger-visibility/" + str(charger_id) + "/0/test@test.com")
     request_answer = requests.get(url=url+"get-chargers-in-range/31.5/21/100").json()
-    for i in range(0, len(request_answer)):
-        if request_answer[i]["id"] == charger_id:
-            raise Exception(request_answer)
-    pass
-
+    if request_answer == []:
+        pass
+    else:
+        for i in range(0, len(request_answer)):
+            if request_answer[i]["id"] == charger_id:          
+                assert not request_answer[i]["id"] == charger_id
 # Sends -1 when changing visibility
 def test_send_minus1():
     charger_id = post_request()
     request_answer = requests.put(url=url+"change-charger-visibility/" + str(charger_id) + "/-1/test@test.com")
-    if request_answer.text == "Visibility already set to true":
-        pass
-    else:
-        raise Exception(request_answer)
+    assert request_answer.text == "Visibility already set to true"
 
 # Sends 2 when changing visibility
 def test_send_2():
     charger_id = post_request()
     request_answer = requests.put(url=url+"change-charger-visibility/" + str(charger_id) + "/2/test@test.com")
-    if request_answer.text == "Visibility already set to true":
-        pass
-    else:
-        raise Exception(request_answer)
+    assert request_answer.text == "Visibility already set to true"
 
 # Sends wrong email when changing visibility
 def test_incorrect_email():
     charger_id = post_request()
     request_answer = requests.put(url=url+"change-charger-visibility/" + str(charger_id) + "/0/test2@test.com")
-    if request_answer.text == "Email or id is incorrect":
-        pass
-    else:
-        raise Exception(request_answer)
+    assert request_answer.text == "Email or id is incorrect"
 
 # Looks if newly added charger can be found
 def test_new_charger_can_be_found():
@@ -165,9 +138,8 @@ def test_new_charger_can_be_found():
     request_answer = requests.get(url=url+"get-chargers-in-range/31.5/21/1000").json()
     for i in range(0, len(request_answer)):
         if request_answer[i]["id"] == charger_id:
-            pass
             return
-    raise Exception(request_answer)
+    assert request_answer[i]["id"] == charger_id
 
 # Looks if inactive charger can be found by email
 def test_inactive_found_by_mail():
@@ -176,24 +148,17 @@ def test_inactive_found_by_mail():
     request_answer = requests.get(url=url+"get-charger-by-email/test@test.com").json()
     for i in range(0, len(request_answer)):
         if request_answer[i]["id"] == charger_id:
-            pass
             return
-    raise Exception(request_answer)
+    assert request_answer[i]["id"] == charger_id
 
 # Sends HAMSTERPAJ when changing visibility
 def test_send_hamsterpaj():
     charger_id = post_request()
     request_answer = requests.put(url=url+"change-charger-visibility/" + str(charger_id) + "/HAMSTERPAJ/test@test.com")
-    if request_answer.text == "Visibility already set to true":
-        pass
-    else:
-        raise Exception(request_answer)
+    assert request_answer.text == "Visibility already set to true"
 
 # Sends wierd id when changing visibility
 def test_send_wierd_charger_id():
     expected_answer = {"errors": [{"value": "t\"--","msg": "Must be a number!","param": "id","location": "params"}]}
     request_answer = requests.put(url=url+'change-charger-visibility/t"--/1/test@test.com')
-    if request_answer.json() == expected_answer:
-        pass
-    else:
-        raise Exception(request_answer)
+    assert request_answer.json() == expected_answer
