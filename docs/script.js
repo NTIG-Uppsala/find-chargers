c6dd3fb8195f65ab71f2a5747e465e84979affe4
Jonatan Möne <jonatan.mone@elev.ga.ntig.se>
2022-01-25 13:56:31 +0100

    Added tests and changed variable names to be compatible with them.

--- docs/script.js
+++ docs/script.js
@@ -1,65 +1,60 @@
 window.onload = function() {
     mapboxgl.accessToken =
     'pk.eyJ1Ijoiam9lbG55bWFyayIsImEiOiJja3dhanhmcWUxMnNhMm9xbWVrczBjbXRrIn0.RkjM1Q1hRCJMJkgNjXNiFg';
-    const pos0 = [17.645434131111752, 59.8584748398551];
-    const pos1 = [17.635565820824436, 59.85340378431419]
-    const pos2 = [17.639914575310478, 59.848696472545214]
-    const pos3 = [17.649801703165753, 59.84012790068371]
+    const pos1 = [17.645434131111752, 59.8584748398551];
+    const pos2 = [17.635565820824436, 59.85340378431419]
+    const pos3 = [17.639914575310478, 59.848696472545214]
+    const pos4 = [17.649801703165753, 59.84012790068371]
     const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/joelnymark/ckyijeu1i4p1615o8ldlbo1x0',
-    center: pos0,
+    center: pos1,
     zoom: 13
     });
 
 
 
     // create the popup
-    const popup = new mapboxgl.Popup({
+    const popup1 = new mapboxgl.Popup({
     offset: 38
     }).setHTML(
     '<h3>UPPSALA CENTRALSTATION</h3><p>Laddartyp: AC1, AC2</p> <p>Tillgänglig : Ledig!</p> <button onclick="pinInfo(1);" style="width: 80px;, hight:60px;"id="view-full-information">View Full'
     )
-    const popup1 = new mapboxgl.Popup({
+    const popup2 = new mapboxgl.Popup({
     offset: 38
     }).setHTML(
     '<h3>UPPSALA SLOTT</h3><p>Laddartyp: AC2</p> <p>Tillgänglig : Ledig!</p> <button onclick="pinInfo(2);" style="width: 80px;, hight:60px;"id="view-full-information">View Full'
     )
-    const popup2 = new mapboxgl.Popup({
+    const popup3 = new mapboxgl.Popup({
     offset: 38
     }).setHTML(
     '<h3>UPPSALA SJUKHUS</h3><p>Laddartyp: ChaDemo</p> <p>Tillgänglig : UPTAGEN!</p> <button onclick="pinInfo(3);" style="width: 80px;, hight:60px;"id="view-full-information">View Full'
     )
-    const popup3 = new mapboxgl.Popup({
+    const popup4 = new mapboxgl.Popup({
     offset: 38
     }).setHTML(
     '<h3>NTI UPPSALA</h3><p>Laddartyp: AC2</p> <p>Tillgänglig : Ledig!</p> <button onclick="pinInfo(4);" style="width: 80px;, hight:60px;"id="view-full-information">View Full'
     )
 
     // create DOM element for the marker
-    const el = document.createElement('div');
-    el.id = 'marker0';
-
     const el1 = document.createElement('div');
-    el1.id = 'marker1'
+    el1.id = 'marker1';
 
     const el2 = document.createElement('div');
     el2.id = 'marker2'
 
     const el3 = document.createElement('div');
     el3.id = 'marker3'
+
+    const el4 = document.createElement('div');
+    el4.id = 'marker4'
     
     // create the marker
-    new mapboxgl.Marker(el)
-    .setLngLat(pos0)
-    .setPopup(popup) // sets a popup on this marker
-    .addTo(map);
-    
     new mapboxgl.Marker(el1)
     .setLngLat(pos1)
     .setPopup(popup1) // sets a popup on this marker
     .addTo(map);
-
+    
     new mapboxgl.Marker(el2)
     .setLngLat(pos2)
     .setPopup(popup2) // sets a popup on this marker
@@ -69,12 +64,17 @@ window.onload = function() {
     .setLngLat(pos3)
     .setPopup(popup3) // sets a popup on this marker
     .addTo(map);
+
+    new mapboxgl.Marker(el4)
+    .setLngLat(pos4)
+    .setPopup(popup4) // sets a popup on this marker
+    .addTo(map);
     
     const classthing = "marker"
-    document.getElementById("marker0").classList.add(classthing)
     document.getElementById("marker1").classList.add(classthing)
     document.getElementById("marker2").classList.add(classthing)
     document.getElementById("marker3").classList.add(classthing)
+    document.getElementById("marker4").classList.add(classthing)
 }
 
 function closeSideNav(){
---
+++ tests/prototype_tests.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver.get("https://www.google.com")
+website = "file:///C:/Users/jonatan.mone/Projects/find-chargers/docs/index.html"#"https://tinyurl.com/Find-Charger"
+driver.get(website)
+time.sleep(1)
+
+#Clicks all the arrow buttons.
+for i in range(1, 5):
+    button = driver.find_element(By.ID, "{}".format(i))
+    button.click()
+time.sleep(1)
+
+#Clicks the menu button twice to see if arrow buttons reset, as they should and then hides the menu.
+menu_button = driver.find_element(By.CSS_SELECTOR, "div#btn-nav > button")
+for i in range(0, 2):
+    menu_button.click()
+time.sleep(1)
+menu_button.click()
+
+#Clicks all the map markers and their view full button twice.
+for i in range(0, 2):
+    for j in range(1, 5):
+        map_marker = driver.find_element(By.ID, "marker{}".format(j))
+        map_marker.click()
+        time.sleep(0.25)
+        view_full_button = driver.find_element(By.ID, "view-full-information")
+        view_full_button.click()
+
+time.sleep(1)
